fd0e2f5546fbde742c1667975541babb19de2a9a
Liubomyr Halamaha <galamagal86@gmail.com>
2026-06-09 14:08:59 +0300

    fix

--- config/env.ts
+++ config/env.ts
@@ -4,7 +4,9 @@ import path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const BASE_URL: string | undefined = process.env.BASE_URL;
+const HEADLESS: boolean = process.env.HEADLESS !== 'false';
 
 export default {
   BASE_URL,
+  HEADLESS,
 };
