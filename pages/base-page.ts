17cc2b5a0e5b3624e235d74fc93c08ed7432a399
DmytroSyadro <dmitry.syadro@gmail.com>
2026-06-14 16:36:47 +0300

    feat: switching methods in the base page class

--- pages/base-page.ts
+++ pages/base-page.ts
@@ -1,8 +1,9 @@
-import type { Locator, Page } from '@playwright/test';
+import type { BrowserContext, Locator, Page } from '@playwright/test';
 import { SearchBarComponent } from './components/search-bar-component';
 
 export abstract class BasePage {
   protected page: Page;
+  protected context: BrowserContext;
   protected searchBar: SearchBarComponent;
 
   private readonly searchBarLocator: Locator;
@@ -11,6 +12,7 @@ export abstract class BasePage {
     this.page = page;
     this.searchBarLocator = page.locator("//div[@class='search']");
     this.searchBar = new SearchBarComponent(this.searchBarLocator);
+    this.context = page.context();
   }
 
   async navigateTo(url: string): Promise<void> {
@@ -23,5 +25,22 @@ export abstract class BasePage {
 
   async waitForPageLoad(): Promise<void> {
     await this.page.waitForLoadState('load');
+  }
+
+  async switchToNewTab(): Promise<Page> {
+    const [newPage] = await Promise.all([this.context.waitForEvent('page')]);
+    await newPage.waitForLoadState();
+    return newPage;
+  }
+  async getAllPage(): Promise<Page[]> {
+    return this.context.pages();
+  }
+  async switchToTabByIndex(index: number): Promise<Page> {
+    const pages = this.context.pages();
+    await pages[index].bringToFront();
+    return pages[index];
+  }
+  async closeCurrentTab(): Promise<void> {
+    await this.page.close();
   }
 }
