3cb3ec921c7e6df2b2ac4043982fad723cffa605
BogdanBorsukevich <borsukevich.bogdan173@gmail.com>
2026-06-18 23:51:12 +0300

    Changings in challenge-carousel.spec.ts

--- tests/challenge-carousel.spec.ts
+++ tests/challenge-carousel.spec.ts
@@ -19,27 +19,21 @@ test.describe('Challenge page Tasks Carousel', () => {
     expect(allCards.length).toBeGreaterThan(0);
 
     await expect(activeCards.first()).toBeVisible();
-    const activeCount = await activeCards.count();
-    expect(activeCount).toBe(4);
+    await expect(activeCards).toHaveCount(4);
 
     const firstActiveCardName = await activeCardName.textContent();
     await carousel.clickNextArrow();
     await expect(activeCardName).not.toHaveText(firstActiveCardName!);
 
-    const firstCardNameAfterNext = await activeCardName.textContent();
-    expect(firstCardNameAfterNext).not.toBe(firstActiveCardName);
+    await expect(activeCardName).not.toHaveText(firstActiveCardName!);
 
     await carousel.clickPrevArrow();
     await expect(activeCardName).toHaveText(firstActiveCardName!);
-
-    const firstCardNameAfterPrev = await activeCardName.textContent();
-    expect(firstCardNameAfterPrev).toBe(firstActiveCardName);
 
     await carousel.clickCarouselDot(2);
     await expect(activeCardName).not.toHaveText(firstActiveCardName!);
 
     await expect(activeCards.first()).toBeVisible();
-    const activeCountAfterDot = await activeCards.count();
-    expect(activeCountAfterDot).toBeGreaterThan(0);
+    await expect(activeCards).toHaveCount(4);
   });
 });
