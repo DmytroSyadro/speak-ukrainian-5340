54180520ec76c1b375403f909b8e3645a2bfb75e
DmytroSyadro <dmitry.syadro@gmail.com>
2026-06-19 13:27:49 +0300

    fix set viewport

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,6 +17,10 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
+    viewport: {
+      width: 1920,
+      height: 1080,
+    },
     baseURL: env.BASE_URL,
     headless: env.HEADLESS,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
