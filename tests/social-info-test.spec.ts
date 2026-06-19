a40f30b41984005c4c5ac42741296ae95e65eb93
Lesia <lesia.liashko.shi.2024@lpnu.ua>
2026-06-19 10:43:28 +0300

    implem ent test case #43

---
+++ tests/social-info-test.spec.ts
@@ -0,0 +1,21 @@
+import {test, expect} from '@playwright/test';
+import {ChallengePage} from '@/pages/challenge-page';
+
+test.describe('Check Social Media and Donation links routing on the Challenge page', () => {
+  test('[TC-043] Verify Social Media and Donation links routing on the Challenge page', 
+    async ({ page }) => {
+    const challengePage = new ChallengePage(page);
+    const challengeId = 5; 
+
+    await challengePage.goto(challengeId);
+    
+    const socialInfoComponent = challengePage.getSocialInfoComponent();
+    await expect(socialInfoComponent.getSocialLinkHref('facebook')).resolves.toBe('https://www.facebook.com/teach.in.ukrainian');
+    await expect(socialInfoComponent.getSocialLinkHref('youtube')).resolves.toBe('https://www.youtube.com/channel/UCP38C0jxC8aNbW34eBoQKJw');
+    await expect(socialInfoComponent.getSocialLinkHref('instagram')).resolves.toBe('https://www.instagram.com/yedyni.ruh/');
+    await expect(socialInfoComponent.getSocialLinkHref('mail')).resolves.toBe('mailto:teach.in.ukrainian@gmail.com');
+    
+    await challengePage.clickHelpButton();
+    const newPage = await challengePage.switchToNewTab();
+    await expect(newPage.url()).toBe('https://secure.wayforpay.com/payment/s0f2891d77061');
+    })})
