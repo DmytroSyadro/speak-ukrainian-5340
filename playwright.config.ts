24bdb55bf06609b066e7a89c74da74ab5212ad81
Liubomyr Halamaha <galamagal86@gmail.com>
2026-06-23 21:15:07 +0300

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig, devices } from '@playwright/test';
-import env from './config/env';
-import { TEST_TIMEOUTS } from './config/test-timeouts';
+import env from '@/config/env';
+
+process.env.allure_quiet = 'true';
+process.env.QUIET = 'true';
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: './tests',
-  timeout: TEST_TIMEOUTS.defaultE2E,
+  timeout: env.TEST_TIMEOUTS.defaultE2E,
   expect: {
-    timeout: TEST_TIMEOUTS.expect,
+    timeout: env.TEST_TIMEOUTS.expect,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -18,23 +21,23 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
     viewport: {
       width: 1920,
       height: 1080,
     },
     baseURL: env.BASE_URL,
     headless: env.HEADLESS,
-    actionTimeout: TEST_TIMEOUTS.action,
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    actionTimeout: env.TEST_TIMEOUTS.action,
+    trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: 'off',
     ignoreHTTPSErrors: true,
   },
+
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
     [
@@ -70,44 +73,17 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], viewport: { width: 1920, height: 1080 } },
+      use: { ...devices['Desktop Chrome'] },
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'], viewport: { width: 1920, height: 1080 } },
+      use: { ...devices['Desktop Firefox'] },
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'], viewport: { width: 1920, height: 1080 } },
+      use: { ...devices['Desktop Safari'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
