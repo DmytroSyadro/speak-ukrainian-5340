af3ee4f5083ed33cd641269832be4291a8f1194a
Tonnnya <53794265+Tonnnya@users.noreply.github.com>
2026-06-26 10:48:15 +0300

    fix

--- modals/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './club-modal';
-export * from './comment-modal';
-export * from './map-modal';
-export * from './authorization/sign-in-modal';
-export * from './authorization/sign-up-modal';
